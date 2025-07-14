aa54f2a307d9ca276e0bbd93d7059de878f47e58
Cristian Nahuas <crnahuas@gmail.com>
2025-07-14 18:55:31 -0400

    Verifica si esPrimo

--- SafeVoteSystem/src/main/java/com/crnahuas/core/PrimesList.java
+++ SafeVoteSystem/src/main/java/com/crnahuas/core/PrimesList.java
@@ -1,5 +1,23 @@
 package com.crnahuas.core;
 
-class PrimesList {
-    
+import java.util.ArrayList;
+
+// Lista especializada que solo acepta números primos.
+// Restringe la inserción de no primos.
+public class PrimesList extends ArrayList<Integer> {
+
+    // Verifica si un número es primo
+    public boolean isPrime(int number) {
+        if (number < 2) {
+            return false; // No hay primos menores que 2
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Tiene divisor → no es primo
+            }
+        }
+        return true;
+    }
+
+
 }
